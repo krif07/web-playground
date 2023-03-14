e6caa010821026bde3a8095df68de9d1bdc3b78c
Cristian Fernando Dávila López <krif07@gmail.com>
2023-03-13 23:43:56 -0500

    CRUD Update view

--- webplayground/pages/urls.py
+++ webplayground/pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PageListView, PageDetailView, PageCreateView
+from .views import PageListView, PageDetailView, PageCreateView, PageUpdateView
 
 pages_patterns = ([
     path('', PageListView.as_view(), name='pages'),
     path('<int:pk>/<slug:slug>/', PageDetailView.as_view(), name='page'),
     path('create/', PageCreateView.as_view(), name='create'),
+    path('update/<int:pk>/', PageUpdateView.as_view(), name='update'),
 ], 'pages')
--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from .models import Page
 
@@ -15,3 +15,11 @@ class PageCreateView(CreateView):
     model = Page
     fields = ['title', 'content', 'order']
     success_url = reverse_lazy('pages:pages')
+
+class PageUpdateView(UpdateView):
+    model = Page
+    fields = ['title', 'content', 'order']
+    template_name_suffix = '_update_form'
+    success_url = reverse_lazy('pages:pages')
+    def get_success_url(self):
+        return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
