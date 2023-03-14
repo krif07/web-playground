f4a99b282a4f34951d7bb533f9df35d1751a5015
Cristian Fernando Dávila López <krif07@gmail.com>
2023-03-13 23:23:33 -0500

    CRUD Update view

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -1,10 +1,17 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Page
 
 # Create your views here.
 class PageListView(ListView):
     model = Page
-    
+
 class PageDetailView(DetailView):
    model = Page
+
+class PageCreateView(CreateView):
+    model = Page
+    fields = ['title', 'content', 'order']
+    success_url = reverse_lazy('pages:pages')
