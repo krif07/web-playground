e6c05d53a0805c21f30273e40b160fd5caf95cbc
Cristian Fernando Dávila López <krif07@gmail.com>
2023-03-13 23:50:54 -0500

    CRUD Delete view

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Page
 
@@ -23,3 +23,7 @@ class PageUpdateView(UpdateView):
     success_url = reverse_lazy('pages:pages')
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
+    
+class PageDeleteView(DeleteView):
+    model = Page
+    success_url = reverse_lazy('pages:pages')
